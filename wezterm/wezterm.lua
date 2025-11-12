28d530d762765862c62dbe5c67c3c44501113a60
Facu <gabylope05@gmail.com>
2025-11-12 11:19:03 -0300

    refactor(wezterm): elimino variables innecesarias

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,20 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-local style = require("config.style")
-local colors = require("config.colors")
-local keys = require("config.keybindings")
+local configuration = {
+  colors = require("config.colors"),
+  keys = require("config.keybindings"),
+  style = require("config.style"),
+}
 
 local tabs = require("config.tabs")
 local utils = require("config.utils")
 
-local M = {
-  colors = colors,
-  keys = keys,
-  style = style,
-}
-
-for _, map in pairs(M) do
+for _, map in pairs(configuration) do
     for k, v in pairs(map) do
         config[k] = v
     end
